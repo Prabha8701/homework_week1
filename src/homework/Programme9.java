53594085943a8f5915bab87274d3ee454aed1268
Prabha8701 <Prabha8701@gmail.com>
2023-05-09 14:27:12 +0100

    Homework week1 submitted

--- src/homework/Programme9.java
+++ src/homework/Programme9.java
@@ -3,9 +3,9 @@ package homework;
 public class Programme9 {
     public static void main(String[] args) {
         int a = -5; int b = 8; int c = 6;
-        System.out.println(a + b * c);
+        System.out.println(a+b*c);
         int d = 55; int e = 9; int f = 9;
-        System.out.println((55 + 9) %9);
+        System.out.println((d+e)%f);
         int p = 20; int q = -3; int r = 5; int s=8;
         System.out.println(p+q*r/s);
         int t=5; int u=15; int v=3; int w=2; int x=8; int y=3;
